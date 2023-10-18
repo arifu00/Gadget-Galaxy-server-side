307ead7bf9606e560e7b9353525c9f0c5b18afe4
arifu00 <arifu5758@gmail.com>
2023-10-18 12:45:23 +0600

    added totalbrands server side data part

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// gadget-galaxy
+// WMpqnM6kXA34vL1F
 // middle-ware
 
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  "mongodb+srv://gadget-galaxy:WMpqnM6kXA34vL1F@cluster0.kalsdro.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const totalBrands = client.db("gadgetGalaxyDB").collection("brands");
+
+    // brands API
+    app.get("/brands", async (req, res) => {
+      const cursor = totalBrands.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.post("/brands", async (req, res) => {
+      const brands = req.body;
+      console.log(brands);
+      const result = await totalBrands.insertOne(brands);
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hlw World");
