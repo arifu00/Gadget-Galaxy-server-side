a5b1e07318e70fd47ea211ae98b2449a67498909
arifu00 <arifu5758@gmail.com>
2023-10-20 00:34:11 +0600

    product add server side

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// gadget-galaxy
-// WMpqnM6kXA34vL1F
 // middle-ware
 
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://gadget-galaxy:WMpqnM6kXA34vL1F@cluster0.kalsdro.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.DB_PASS}@cluster0.kalsdro.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,6 +27,8 @@ async function run() {
     await client.connect();
 
     const totalBrands = client.db("gadgetGalaxyDB").collection("brands");
+    const productCollection = client.db("gadgetGalaxyDB").collection("products");
+
 
     // brands API
     app.get("/brands", async (req, res) => {
@@ -36,10 +36,32 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/brands/:brandName", async (req, res) => {
+      const brandName = req.params.brandName;
+      const query = { brandName: brandName };
+      const result = await totalBrands.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
     app.post("/brands", async (req, res) => {
       const brands = req.body;
       console.log(brands);
       const result = await totalBrands.insertOne(brands);
+      res.send(result);
+    });
+
+    //  Products API
+
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await productCollection.insertOne(newProduct);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
